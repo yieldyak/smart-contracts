03653254cc53f8ec3b34e4239777a5cd4ac43189
snow-farmer <team@snowballs.finance>
2021-04-08 23:28:04 +0200

    Olive contracts

---
+++ contracts/YakERC20.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./lib/SafeMath.sol";
+import "./interfaces/IERC20.sol";
+
+abstract contract YakERC20 {
+    using SafeMath for uint256;
+
+    string public name = "Yield Yak";
+    string public symbol = "YRT";
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+  
+    mapping (address => mapping (address => uint256)) internal allowances;
+    mapping (address => uint256) internal balances;
+
+    /// keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
+    bytes32 public constant DOMAIN_TYPEHASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
+
+    /// keccak256("1");
+    bytes32 public constant VERSION_HASH = 0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
+
+    /// keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    mapping(address => uint) public nonces;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {}
+
+    /**
+     * @notice Get the number of tokens `spender` is approved to spend on behalf of `account`
+     * @param account The address of the account holding the funds
+     * @param spender The address of the account spending the funds
+     * @return The number of tokens approved
+     */
+    function allowance(address account, address spender) external view returns (uint) {
+        return allowances[account][spender];
+    }
+
+    /**
+     * @notice Approve `spender` to transfer up to `amount` from `src`
+     * @dev This will overwrite the approval amount for `spender`
+     * and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
+     * It is recommended to use increaseAllowance and decreaseAllowance instead
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     * @return Whether or not the approval succeeded
+     */
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    /**
+     * @notice Get the number of tokens held by the `account`
+     * @param account The address of the account to get the balance of
+     * @return The number of tokens held
+     */
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transfer(address dst, uint256 amount) external returns (bool) {
+        _transferTokens(msg.sender, dst, amount);
+        return true;
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `src` to `dst`
+     * @param src The address of the source account
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transferFrom(address src, address dst, uint256 amount) external returns (bool) {
+        address spender = msg.sender;
+        uint256 spenderAllowance = allowances[src][spender];
+
+        if (spender != src && spenderAllowance != uint256(-1)) {
+            uint256 newAllowance = spenderAllowance.sub(amount, "transferFrom: transfer amount exceeds allowance");
+            allowances[src][spender] = newAllowance;
+
+            emit Approval(src, spender, newAllowance);
+        }
+
+        _transferTokens(src, dst, amount);
+        return true;
+    }
+
+
+    /**
+     * @notice Approval implementation
+     * @param owner The address of the account which owns tokens
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     */
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "_approve::owner zero address");
+        require(spender != address(0), "_approve::spender zero address");
+        allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @notice Transfer implementation
+     * @param from The address of the account which owns tokens
+     * @param to The address of the account which is receiving tokens
+     * @param value The number of tokens that are being transferred
+     */
+    function _transferTokens(address from, address to, uint256 value) internal {
+        require(to != address(0), "_transferTokens: cannot transfer to the zero address");
+
+        balances[from] = balances[from].sub(value, "_transferTokens: transfer exceeds from balance");
+        balances[to] = balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function _mint(address to, uint256 value) internal {
+        totalSupply = totalSupply.add(value);
+        balances[to] = balances[to].add(value);
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint256 value) internal {
+        balances[from] = balances[from].sub(value, "_burn: burn amount exceeds from balance");
+        totalSupply = totalSupply.sub(value, "_burn: burn amount exceeds total supply");
+        emit Transfer(from, address(0), value);
+    }
+
+    /**
+     * @notice Triggers an approval from owner to spender
+     * @param owner The address to approve from
+     * @param spender The address to be approved
+     * @param value The number of tokens that are approved (2^256-1 means infinite)
+     * @param deadline The time at which to expire the signature
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, "permit::expired");
+
+        bytes32 encodeData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
+        _validateSignedData(owner, encodeData, v, r, s);
+
+        _approve(owner, spender, value);
+    }
+
+    /**
+     * @notice Recovers address from signed data and validates the signature
+     * @param signer Address that signed the data
+     * @param encodeData Data signed by the address
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function _validateSignedData(address signer, bytes32 encodeData, uint8 v, bytes32 r, bytes32 s) internal view {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                getDomainSeparator(),
+                encodeData
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        // Explicitly disallow authorizations for address(0) as ecrecover returns address(0) on malformed messages
+        require(recoveredAddress != address(0) && recoveredAddress == signer, "Arch::validateSig: invalid signature");
+    }
+
+    /**
+     * @notice EIP-712 Domain separator
+     * @return Separator
+     */
+    function getDomainSeparator() public view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                VERSION_HASH,
+                _getChainId(),
+                address(this)
+            )
+        );
+    }
+
+    /**
+     * @notice Current id of the chain where this contract is deployed
+     * @return Chain id
+     */
+    function _getChainId() internal pure returns (uint) {
+        uint256 chainId;
+        assembly { chainId := chainid() }
+        return chainId;
+    }
+}
