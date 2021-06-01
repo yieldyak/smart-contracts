d1f129f3f3f93e9c427ea404f637b235e6041dbf
SlowCheetah <84160059+ASlowCheetah@users.noreply.github.com>
2021-06-01 01:38:08 +0100

    created DexStrategyV5 which optimizes gas by removing the router

--- contracts/interfaces/IPangolinPair.sol
+++ contracts/interfaces/IPangolinPair.sol
@@ -6,4 +6,7 @@ import "./IPangolinERC20.sol";
 interface IPangolinPair is IPangolinERC20 {
     function token0() external pure returns (address);
     function token1() external pure returns (address);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function mint(address to) external returns (uint liquidity);
 }
