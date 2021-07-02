24c460c068c1ccee7e661078e9dd44f3ada25c0f
sidduHERE <elugamsudhiswar@gmail.com>
2021-07-02 18:16:25 +0530

    Shortened Zap call

--- contracts/AvaxZap.sol
+++ contracts/AvaxZap.sol
@@ -4,18 +4,17 @@ pragma solidity ^0.7.0;
 import "./interfaces/IWAVAX.sol";
 import "./lib/Ownable.sol";
 import "./lib/SafeMath.sol";
+import "./YakStrategy.sol";
 
 /**
  * @notice 
- * @dev Assumes User already sent AVAX to this contract and this contract is being executed from a strategy Contract
+ * @dev Assumes User send AVAX to this contract in value of the payable method
  */
 contract AvaxZap is Ownable {
     using SafeMath for uint;
     address public WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
     bytes private constant zeroBytes = new bytes(0);
     event Received(address,uint);
-    event Deposit(address, uint);
-    event Withdraw(address, uint);
 
     constructor () {}
 
@@ -23,37 +22,16 @@ contract AvaxZap is Ownable {
         emit Received(msg.sender, msg.value);
     }
 
-    function _depositAVAX(uint amountAVAX) external payable {
-        require(payable(address(this)).balance > amountAVAX );
-        IWAVAX(WAVAX).deposit{value: amountAVAX}();
-        require(IWAVAX(WAVAX).transfer(msg.sender, amountAVAX));
-        emit Deposit(msg.sender, amountAVAX);
-    }
-    
-    function _depositAVAXFor(address strategyContract, uint amountAVAX) external payable {
-        require(payable(address(this)).balance > amountAVAX );
-        IWAVAX(WAVAX).deposit{value: amountAVAX}();
-        require(IWAVAX(WAVAX).transfer(address(strategyContract), amountAVAX));
-        emit Deposit(strategyContract, amountAVAX);
+    function _depositAVAX(address strategyContract) external payable {
+        require(payable(address(this)).balance > msg.value );
+        IWAVAX(WAVAX).deposit{value: msg.value}();
+        IWAVAX(WAVAX).approve(strategyContract, msg.value);
+        require(YakStrategy(strategyContract).depositToken.address == address(WAVAX));
+        YakStrategy(strategyContract).depositFor(msg.sender, msg.value);
     }
 
-/**
-     * @notice Safely transfer AVAX
-     * @dev Requires token to return true on transfer
-     * @param to recipient address
-     * @param intValue amount
-     */
-    function _safeTransferAVAX(address payable to, uint256 intValue) internal {
-        (bool success, ) = to.call{value: intValue}(zeroBytes);
-        require(success, 'TransferHelper: AVAX_TRANSFER_FAILED');
-    }
-
-    function withdrawAVAX(uint amountAVAX) external {
-        if (amountAVAX > 0) {
-            IWAVAX(WAVAX).withdraw(amountAVAX);
-            _safeTransferAVAX(msg.sender, amountAVAX);
-            emit Withdraw(msg.sender, amountAVAX);
-        }
+    function Withdraw(address strategyContract, uint amountAVAX) external {
+            YakStrategy(strategyContract).withdraw(amountAVAX);
     }
 
 }
