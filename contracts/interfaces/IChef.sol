496dc6417398bb9953b7e74fd96e0e68528416e7
snow-farmer <team@snowballs.finance>
2021-07-04 16:36:06 +0200

    Simplify JoeChef interface

--- contracts/interfaces/IChef.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-interface IChef {
-    function deposit(uint256 _pid, uint256 _amount) external;
-    function withdraw(uint256 _pid, uint256 _amount) external;
-    function emergencyWithdraw(uint256 _pid) external;
-}
--- contracts/interfaces/IJoeChef.sol
+++ contracts/interfaces/IJoeChef.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
-import "./IChef.sol";
-
-interface IJoeChef is IChef {
+interface IJoeChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function emergencyWithdraw(uint256 _pid) external;
     function pendingTokens(uint256 _pid, address _user) external view returns (uint256 pendingJoe, address bonusTokenAddress, string memory bonusTokenSymbol, uint256 pendingBonusToken);
     function userInfo(uint pid, address user) external view returns (
         uint256 amount,
