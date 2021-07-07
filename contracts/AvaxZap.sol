4a68cdaffb547cf0ad969498906d1d5345861b6d
sidduHERE <elugamsudhiswar@gmail.com>
2021-07-08 01:28:05 +0530

    Removing Withdraw

--- contracts/AvaxZap.sol
+++ contracts/AvaxZap.sol
@@ -8,12 +8,11 @@ import "./YakStrategy.sol";
 
 /**
  * @notice 
- * @dev Assumes User send AVAX to this contract in value of the payable method
+ * @dev Assumes User send AVAX to this contract in value of the payable deposit method
  */
 contract AvaxZap is Ownable {
     using SafeMath for uint;
     address public WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
-    bytes private constant zeroBytes = new bytes(0);
     event Received(address,uint);
 
     constructor () {}
@@ -23,15 +22,11 @@ contract AvaxZap is Ownable {
     }
 
     function _depositAVAX(address strategyContract) external payable {
-        require(payable(address(this)).balance > msg.value );
         IWAVAX(WAVAX).deposit{value: msg.value}();
         IWAVAX(WAVAX).approve(strategyContract, msg.value);
-        require(YakStrategy(strategyContract).depositToken.address == address(WAVAX));
+        require(address(YakStrategy(strategyContract).depositToken()) == address(WAVAX));
         YakStrategy(strategyContract).depositFor(msg.sender, msg.value);
     }
 
-    function Withdraw(address strategyContract, uint amountAVAX) external {
-            YakStrategy(strategyContract).withdraw(amountAVAX);
-    }
 
 }
