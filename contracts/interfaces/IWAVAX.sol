0fd1f4540376357e821c2dc58901f76024a54820
sidduHERE <elugamsudhiswar@gmail.com>
2021-07-08 01:30:21 +0530

    Sync

--- contracts/interfaces/IWAVAX.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-interface IWAVAX {
-    function deposit() external payable;
-    function transfer(address to, uint value) external returns (bool);
-    function withdraw(uint) external;
-    function approve(address to, uint value) external returns (bool);
-}
