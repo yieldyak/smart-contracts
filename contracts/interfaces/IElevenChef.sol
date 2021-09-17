38b9ea0014116e13714b42e7269bab802126ea15
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2021-09-17 12:22:23 +0200

    add eleven strategy for lp (#37)
    * add eleven strategy for lp
    
    * switch to wavax rewards
    
    * calculate withdrawal amount
    
    * calculate deposit amount, fix withdrawal amount calculations

---
+++ contracts/interfaces/IElevenChef.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+interface IElevenChef {
+    function userInfo(uint pid, address user) external view returns (
+        uint256 amount,
+        uint256 rewardDebt
+    );
+    function poolInfo(uint pid) external view returns (
+        address lpToken,           
+        uint256 allocPoint,       
+        uint256 lastRewardTime,   
+        uint256 accElevenPerShare 
+    );
+    function poolLength() external view returns (uint256);
+    function getMultiplier(uint256 _from, uint256 _to) external pure returns (uint256);
+    function pendingEleven(uint256 _pid, address _user) external view returns (uint256);
+    function massUpdatePools() external;
+    function updatePool(uint256 _pid) external;
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function emergencyWithdraw(uint256 _pid) external;
+}
