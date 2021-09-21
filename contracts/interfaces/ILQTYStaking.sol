980ad5b49856f469119a45befffbca1c092ffdd8
snow-farmer <team@snowballs.finance>
2021-09-21 22:34:01 +0200

    Unipool strategy

---
+++ contracts/interfaces/ILQTYStaking.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+interface ILQTYStaking {
+    function getPendingETHGain(address _user) external view returns (uint256);
+    function getPendingLUSDGain(address _user) external view returns (uint256);
+    function snapshots(address) external view returns (uint256);
+    function stakes(address) external view returns (uint256);
+    function stake(uint256) external;
+    function unstake(uint256) external;
+}
