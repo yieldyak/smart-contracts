d67e4832bb82f26db0d8c60e4dfed5f2f33f8819
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2021-10-01 17:15:57 +0200

    delete contracts_old

--- contracts_old/interfaces/IElevenQuickStrat.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-interface IElevenQuickStrat {
-    function WITHDRAWAL_FEE() external view returns (uint);
-    function WITHDRAWAL_MAX() external view returns (uint);
-}
--- contracts_old/interfaces/IYakStrategy.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-
-interface IYakStrategy {
-    function depositToken() external view returns (address);
-    function depositFor(address account, uint amount) external;
-}
