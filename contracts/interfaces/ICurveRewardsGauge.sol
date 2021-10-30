3d1cde5f83165814729a7a4f7a4181a45e478757
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2021-10-30 21:31:15 +0200

    add CurveStrategyForLPV2

--- contracts/interfaces/ICurveRewardsGauge.sol
+++ contracts/interfaces/ICurveRewardsGauge.sol
@@ -3,16 +3,36 @@ pragma solidity ^0.7.0;
 
 interface ICurveRewardsGauge {
     function deposit(uint256 _amount) external;
+
     function withdraw(uint256 _amount) external;
-    function balanceOf(address _address) external view returns(uint);
-    function totalSupply() external view returns(uint);
-    function reward_contract() external view returns(address);
-    function reward_tokens(uint token) external view returns(address);
-    function reward_balances(address token) external view returns(uint);
-    function reward_integral(address token) external view returns(uint);
-    function reward_integral_for(address token, address user) external view returns(uint);
-    function claimable_reward_write(address user, address token) external returns(uint);
-    function claimable_reward(address user, address token) external view returns(uint);
+
+    function balanceOf(address _address) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function reward_contract() external view returns (address);
+
+    function reward_tokens(uint256 token) external view returns (address);
+
+    function reward_balances(address token) external view returns (uint256);
+
+    function reward_integral(address token) external view returns (uint256);
+
+    function reward_integral_for(address token, address user)
+        external
+        view
+        returns (uint256);
+
+    function claimable_reward_write(address user, address token)
+        external
+        returns (uint256);
+
+    function claimable_reward(address user, address token)
+        external
+        view
+        returns (uint256);
+
     function claim_rewards() external;
-    function claim_sig() external view returns(bytes memory);
+
+    function claim_sig() external view returns (bytes memory);
 }
---
+++ contracts/interfaces/ICurveStableSwap.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+interface ICurveStableSwap {
+    function calc_token_amount(uint256[3] memory _amounts, bool _is_deposit)
+        external
+        view
+        returns (uint256);
+
+    function add_liquidity(
+        uint256[3] memory _amounts,
+        uint256 _min_mint_amount,
+        bool _use_underlying
+    ) external returns (uint256);
+
+    function underlying_coins(uint256 index) external view returns (address);
+}
