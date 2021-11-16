2a04f93c79ede32e5999e9560c5a20118414ca66
simednba <47319788+simednba@users.noreply.github.com>
2021-11-16 13:13:47 +0100

    Add hurricane swap strategies (#67)
    * Add STrategy for LP
    
    * Add strategy for XHCT
    
    * Add Emergency withdraw
    
    * Add support for pool with HCT
    
    * prettier

---
+++ contracts/interfaces/IHurricaneFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+
+interface IHurricaneFactory {
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
+}
---
+++ contracts/interfaces/IHurricaneMasterChief.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+interface IHurricaneMasterChief {
+    function pending(uint256 pid, address user) external view returns (uint256, uint256);
+
+    function deposit(uint256 pid, uint256 amount) external;
+
+    function withdraw(uint256 pid, uint256 amount) external;
+
+    function emergencyWithdraw(uint256 pid) external;
+
+    function userInfo(uint256 _pid, address _address)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    function LpOfPid(address _address) external view returns (uint256);
+}
