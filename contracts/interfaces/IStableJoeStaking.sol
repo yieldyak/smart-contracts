9cb9bba9b060f287fdf21e68aad8cdc8441df4a2
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2022-02-25 14:45:52 +0100

    add CompoundingJoeV3 (#103)
    * Create MasterChefVariableRewardsStrategyForSA.sol
    
    * add CompoundingJoeV3
    
    * remove unused imports
    
    * adjust IStableJoeStaking to api changes

---
+++ contracts/interfaces/IStableJoeStaking.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.3;
+
+interface IStableJoeStaking {
+    function deposit(uint256 _amount) external;
+
+    function withdraw(uint256 _amount) external;
+
+    function emergencyWithdraw() external;
+
+    function pendingReward(address _user, address _token) external view returns (uint256);
+
+    function rewardTokens(uint256 index) external view returns (address);
+
+    function rewardTokensLength() external view returns (uint256);
+
+    function getUserInfo(address _user, address _rewardToken) external view returns (uint256, uint256);
+
+    function depositFeePercent() external view returns (uint256);
+
+    function DEPOSIT_FEE_PERCENT_PRECISION() external view returns (uint256);
+}
