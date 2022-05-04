c5b892fc38cda785b09808382556b148a8798865
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2022-05-04 14:01:02 +0200

    remove BoosterFeeCollector duplicate

--- contracts/interfaces/IBoosterFeeCollector.sol
+++ contracts/interfaces/IBoosterFeeCollector.sol
@@ -4,11 +4,11 @@ pragma solidity 0.7.3;
 interface IBoosterFeeCollector {
     function setBoostFee(address _strategy, uint256 _boostFeeBips) external;
 
-    function setBoosterFeeReceiver(address _boosterFeeReceiver) external;
-
     function setPaused(bool _paused) external;
 
     function calculateBoostFee(address _strategy, uint256 _amount) external view returns (uint256);
 
     function compound() external;
+
+    function sweepTokens(address tokenAddress, uint256 tokenAmount) external;
 }
