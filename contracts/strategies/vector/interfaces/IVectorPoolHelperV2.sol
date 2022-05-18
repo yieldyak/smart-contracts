3952c0f5ea498818621dc2f61b2292129a05a45d
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2022-05-18 14:39:05 +0200

    add VectorStrategyForSAV2 (#129)
    * remove BoosterFeeCollector duplicate
    
    * add VectorStrategyForSAV2
    
    * read echidna reward pool address from contract (#132)
    
    * fix benqi pending reward calculation (#128)
    
    * Benqi v3 deplyments
    
    * Echidna deployment
    
    * Joe deployments
    
    * Pangolin deployments
    
    * update to solidity 0.8.13 (#134)
    
    * update to solidity 0.8.13
    
    * remove Cycle
    
    * readd HakuStrategyForLP
    
    * readd PairHelper
    
    * Delete AvaxZap.sol
    
    * readd admin fee field and setter
    
    * Delete Blizz
    
    * Remove setAllowances
    
    * Require admin fee to be 0 in case of update
    
    * add VectorStrategyForSAV2
    
    * move VectorStrategyForSAV2 to vector folder
    
    Co-authored-by: snow-farmer <team@snowballs.finance>

---
+++ contracts/strategies/vector/interfaces/IVectorPoolHelperV2.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IVectorPoolHelperV2 {
+    function depositTokenBalance(address _account) external view returns (uint256);
+
+    function mainStaking() external view returns (address);
+
+    function masterVtx() external view returns (address);
+
+    function stakingToken() external view returns (address);
+
+    function earned(address token) external view returns (uint256 vtxAmount, uint256 tokenAmount);
+
+    function deposit(uint256 _amount) external;
+
+    function stake(uint256 _amount) external;
+
+    function withdraw(uint256 amount, uint256 minAmount) external;
+
+    function getReward() external;
+}
