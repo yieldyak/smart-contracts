56075275781c682a1c541a29c3a081900994c82c
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2022-05-18 14:47:51 +0200

    add SteakStrategyForLP (#130)
    * add SteakStrategyForLP
    
    * read echidna reward pool address from contract (#132)
    
    * fix benqi pending reward calculation (#128)
    
    * Benqi v3 deplyments
    
    * Echidna deployment
    
    * Joe deployments
    
    * Pangolin deployments
    
    * update to solidity 0.8.13 (#134)
    
    * update to solidity 0.8.13
    
    * remove Cycle
    
    * readd HakuStrategyForLP
    
    * readd PairHelper
    
    * Delete AvaxZap.sol
    
    * readd admin fee field and setter
    
    * Delete Blizz
    
    * Remove setAllowances
    
    * Require admin fee to be 0 in case of update
    
    * add SteakStrategyForLP
    
    * upgrade SteakStrategyForLP to solidity 0.8.13
    
    Co-authored-by: snow-farmer <team@snowballs.finance>

---
+++ contracts/strategies/steakhut/interfaces/ISteakMasterChef.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface ISteakMasterChef {
+    function deposit(uint256 _pid, uint256 _amount) external;
+
+    function withdraw(uint256 _pid, uint256 _amount) external;
+
+    function userInfo(uint256 _pid, address _user) external view returns (uint256 amount, uint256 rewardDebt);
+
+    function pendingJoe(uint256 _pid, address _user) external view returns (uint256);
+}
