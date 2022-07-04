db9467f98b208c67bbf1bc69b0dfa6f79e53b844
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2022-07-04 17:01:53 +0200

    add SynapseStrategy

---
+++ contracts/strategies/synapse/interfaces/ISwapFlashLoan.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface ISwapFlashLoan {
+    function addLiquidity(
+        uint256[] calldata amounts,
+        uint256 minToMint,
+        uint256 deadline
+    ) external returns (uint256);
+
+    function getTokenIndex(address token) external view returns (uint8);
+}
