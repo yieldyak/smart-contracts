f2f25ae009f4c1b6caa4f1f374e7e65932a50a40
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2023-01-26 12:13:46 +0100

    update WoofiSuperchargerStrategy (#169)

--- contracts/strategies/avalanche/woofi/WoofiSuperchargerStrategy.sol
+++ contracts/strategies/avalanche/woofi/WoofiSuperchargerStrategy.sol
@@ -5,32 +5,43 @@ import "./../../VariableRewardsStrategy.sol";
 import "./interfaces/IMasterChefWoo.sol";
 import "./interfaces/IWooStakingVault.sol";
 import "./interfaces/IWooSuperChargerVault.sol";
+import "./interfaces/IWooPPV2.sol";
+import "./interfaces/IWooRouterV2.sol";
 
 contract WoofiSuperchargerStrategy is VariableRewardsStrategy {
+    using SafeERC20 for IERC20;
+
     uint256 public immutable PID;
     IMasterChefWoo public immutable wooChef;
     IWooStakingVault public immutable xWoo;
-    address public immutable swapPairWavaxUnderlying;
-    address public immutable underlyingAsset;
+    IWooPPV2 public immutable pool;
+    IWooRouterV2 public immutable router;
+    address public immutable rebateCollector;
 
     address public constant WOOe = 0xaBC9547B534519fF73921b1FBA6E672b5f58D083;
 
     constructor(
         address _stakingContract,
         uint256 _pid,
-        address _swapPairWavaxUnderlying,
+        address _router,
+        address _rebateCollector,
         VariableRewardsStrategySettings memory _settings,
         StrategySettings memory _strategySettings
     ) VariableRewardsStrategy(_settings, _strategySettings) {
         wooChef = IMasterChefWoo(_stakingContract);
         xWoo = IWooStakingVault(wooChef.xWoo());
-        underlyingAsset = IWooSuperChargerVault(_strategySettings.depositToken).want();
+        require(
+            address(rewardToken) == IWooSuperChargerVault(_strategySettings.depositToken).want(),
+            "Invalid reward token"
+        );
+        router = IWooRouterV2(_router);
+        pool = IWooPPV2(router.wooPool());
+        rebateCollector = _rebateCollector;
         PID = _pid;
-        swapPairWavaxUnderlying = _swapPairWavaxUnderlying;
     }
 
     receive() external payable {
-        require(underlyingAsset == address(WAVAX) && msg.sender == address(WAVAX), "not allowed");
+        require(address(rewardToken) == address(WAVAX) && msg.sender == address(WAVAX), "not allowed");
     }
 
     function _depositToStakingContract(uint256 _amount, uint256) internal override {
@@ -49,7 +60,10 @@ contract WoofiSuperchargerStrategy is VariableRewardsStrategy {
         uint256 instantWithdrawFee = (pendingWooAmount * xWoo.withdrawFee()) / _bip();
 
         Reward[] memory pendingRewards = new Reward[](1);
-        pendingRewards[0] = Reward({reward: address(WOOe), amount: pendingWooAmount - instantWithdrawFee});
+        pendingRewards[0] = Reward({
+            reward: address(rewardToken),
+            amount: router.querySwap(WOOe, address(rewardToken), pendingWooAmount - instantWithdrawFee)
+        });
 
         return pendingRewards;
     }
@@ -57,27 +71,22 @@ contract WoofiSuperchargerStrategy is VariableRewardsStrategy {
     function _getRewards() internal override {
         wooChef.harvest(PID);
         xWoo.instantWithdraw(xWoo.balanceOf(address(this)));
+        uint256 amount = IERC20(WOOe).balanceOf(address(this));
+        uint256 amountTo = pool.query(WOOe, address(rewardToken), amount);
+        IERC20(WOOe).safeTransfer(address(pool), amount);
+        IWooPPV2(pool).swap(WOOe, address(rewardToken), amount, amountTo, address(this), rebateCollector);
     }
 
     function _convertRewardTokenToDepositToken(uint256 _fromAmount) internal override returns (uint256 toAmount) {
-        if (underlyingAsset != address(rewardToken)) {
-            _fromAmount = DexLibrary.swap(
-                _fromAmount,
-                address(rewardToken),
-                underlyingAsset,
-                IPair(swapPairWavaxUnderlying)
-            );
-        }
-
-        if (underlyingAsset == address(WAVAX)) {
+        if (address(rewardToken) == address(WAVAX)) {
             WAVAX.withdraw(_fromAmount);
             IWooSuperChargerVault(address(depositToken)).deposit{value: _fromAmount}(_fromAmount);
         } else {
-            IERC20(underlyingAsset).approve(address(depositToken), _fromAmount);
+            rewardToken.approve(address(depositToken), _fromAmount);
             IWooSuperChargerVault(address(depositToken)).deposit(_fromAmount);
         }
 
-        return (_fromAmount * 1e18) / IWooSuperChargerVault(address(depositToken)).getPricePerFullShare();
+        return depositToken.balanceOf(address(this));
     }
 
     function totalDeposits() public view override returns (uint256 amount) {
