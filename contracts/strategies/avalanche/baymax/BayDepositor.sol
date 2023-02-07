de2c049282c3bc1fd9e09d5c12dac299d6f77815
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2023-02-07 16:10:19 +0100

    add CompoundingBlp

---
+++ contracts/strategies/avalanche/baymax/BayDepositor.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "../../../lib/Ownable.sol";
+
+import "./interfaces/IBayDepositor.sol";
+
+contract BayDepositor is IBayDepositor, Ownable {
+    address public proxy;
+
+    modifier onlyGmxProxy() {
+        require(msg.sender == proxy, "GmxDepositor::onlyGmxProxy");
+        _;
+    }
+
+    constructor(address _owner) {
+        transferOwnership(_owner);
+    }
+
+    function setGmxProxy(address _proxy) external override onlyOwner {
+        proxy = _proxy;
+    }
+
+    function execute(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) external override onlyGmxProxy returns (bool, bytes memory) {
+        (bool success, bytes memory result) = target.call{value: value}(data);
+
+        return (success, result);
+    }
+}
---
+++ contracts/strategies/avalanche/baymax/interfaces/IBlpProxy.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./IBayDepositor.sol";
+
+interface IBlpProxy {
+    function gmxDepositor() external view returns (IBayDepositor);
+
+    function gmxRewardRouter() external view returns (address);
+
+    function buyAndStakeGlp(uint256 _amount) external returns (uint256);
+
+    function withdrawGlp(uint256 _amount) external;
+
+    function pendingRewards() external view returns (uint256);
+
+    function claimReward() external;
+
+    function totalDeposits() external view returns (uint256);
+
+    function emergencyWithdraw(uint256 _balance) external;
+}
