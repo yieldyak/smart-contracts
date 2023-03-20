3e055cd9ce09be61ca078f037748eb2c01d704fb
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2023-03-20 10:36:11 +0100

    add sushi strategy

---
+++ contracts/strategies/arbitrum/sushi/interfaces/ITreasureRewarder.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface ITreasureRewarder {
+    function pendingTokens(
+        uint256,
+        address,
+        uint256 rewardAmount
+    ) external view returns (address[] memory rewardTokens, uint256[] memory rewardAmounts);
+}
