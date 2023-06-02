9d25a0ae6bd14f55bc51eebf1af2fe0d1799a125
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2023-06-02 11:44:47 +0200

    add sushi strategy (#185)

---
+++ contracts/strategies/arbitrum/sushi/interfaces/IMiniChefV2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IMiniChefV2 {
+    function userInfo(uint256 pid, address user) external view returns (uint256 amount, uint256 rewardDebt);
+
+    function poolInfo(uint256 pid)
+        external
+        view
+        returns (
+            uint256 allocPoint,
+            uint256 lastRewardTime,
+            uint256 accRewardPerShare
+        );
+
+    function rewarder(uint256 pid) external view returns (address);
+
+    function lpToken(uint256 pid) external view returns (address);
+
+    function pendingSushi(uint256 _pid, address _user) external view returns (uint256);
+
+    function deposit(
+        uint256 pid,
+        uint256 amount,
+        address to
+    ) external;
+
+    function withdraw(
+        uint256 pid,
+        uint256 amount,
+        address to
+    ) external;
+
+    function harvest(uint256 pid, address to) external;
+
+    function emergencyWithdraw(uint256 pid, address to) external;
+}
