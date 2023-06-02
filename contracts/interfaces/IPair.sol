764a861567e05e6975e740242bad5d94a4976e33
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2023-06-02 12:07:13 +0200

    add support for Glacier stable pairs (#186)

--- contracts/interfaces/IPair.sol
+++ contracts/interfaces/IPair.sol
@@ -8,23 +8,13 @@ interface IPair is IERC20 {
 
     function token1() external pure returns (address);
 
-    function swap(
-        uint256 amount0Out,
-        uint256 amount1Out,
-        address to,
-        bytes calldata data
-    ) external;
+    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
 
-    function getReserves()
-        external
-        view
-        returns (
-            uint112 reserve0,
-            uint112 reserve1,
-            uint32 blockTimestampLast
-        );
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
 
     function mint(address to) external returns (uint256 liquidity);
 
     function sync() external;
+
+    function stable() external view returns (bool);
 }
--- contracts/strategies/avalanche/glacier/GlacierStrategy.sol
+++ contracts/strategies/avalanche/glacier/GlacierStrategy.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.13;
 import "./../../VariableRewardsStrategy.sol";
 
 import "./interfaces/IGauge.sol";
+import "./interfaces/IRouter.sol";
 import "./interfaces/IGlacierBoosterFeeCollector.sol";
 
 contract GlacierStrategy is VariableRewardsStrategy {
@@ -11,6 +12,7 @@ contract GlacierStrategy is VariableRewardsStrategy {
 
     struct GlacierStrategySettings {
         address gauge;
+        address router;
         address swapPairToken0;
         uint256 swapFeeToken0;
         address swapPairToken1;
@@ -20,7 +22,14 @@ contract GlacierStrategy is VariableRewardsStrategy {
 
     address internal constant GLCR = 0x3712871408a829C5cd4e86DA1f4CE727eFCD28F6;
 
+    address internal immutable token0;
+    address internal immutable token1;
+    uint256 internal immutable token0Decimals;
+    uint256 internal immutable token1Decimals;
+    bool internal immutable stablePair;
+
     IGauge public immutable gauge;
+    IRouter public immutable router;
 
     IGlacierBoosterFeeCollector public boosterFeeCollector;
     address public swapPairToken0;
@@ -34,11 +43,17 @@ contract GlacierStrategy is VariableRewardsStrategy {
         StrategySettings memory _strategySettings
     ) VariableRewardsStrategy(_variableRewardsStrategySettings, _strategySettings) {
         gauge = IGauge(_glacierStrategySettings.gauge);
+        router = IRouter(_glacierStrategySettings.router);
         swapPairToken0 = _glacierStrategySettings.swapPairToken0;
         swapPairToken1 = _glacierStrategySettings.swapPairToken1;
         swapFeeToken0 = _glacierStrategySettings.swapFeeToken0;
         swapFeeToken1 = _glacierStrategySettings.swapFeeToken1;
         boosterFeeCollector = IGlacierBoosterFeeCollector(_glacierStrategySettings.boosterFeeCollector);
+        token0 = IPair(address(depositToken)).token0();
+        token1 = IPair(address(depositToken)).token1();
+        token0Decimals = 10 ** IERC20(token0).decimals();
+        token1Decimals = 10 ** IERC20(token1).decimals();
+        stablePair = IPair(address(depositToken)).stable();
     }
 
     function updateBoosterFeeCollector(address _collector) external onlyDev {
@@ -98,15 +113,39 @@ contract GlacierStrategy is VariableRewardsStrategy {
     }
 
     function _convertRewardTokenToDepositToken(uint256 fromAmount) internal override returns (uint256 toAmount) {
-        toAmount = DexLibrary.convertRewardTokensToDepositTokens(
-            fromAmount,
-            address(rewardToken),
-            address(depositToken),
-            IPair(swapPairToken0),
-            swapFeeToken0,
-            IPair(swapPairToken1),
-            swapFeeToken1
-        );
+        if (stablePair) {
+            uint256 amountIn = fromAmount / 2;
+            uint256 token0Out = DexLibrary.estimateConversionThroughPair(
+                amountIn, address(rewardToken), token0, IPair(swapPairToken0), swapFeeToken0
+            );
+            uint256 token1Out = DexLibrary.estimateConversionThroughPair(
+                amountIn, address(rewardToken), token1, IPair(swapPairToken1), swapFeeToken1
+            );
+            (uint256 quote0, uint256 quote1,) =
+                IRouter(router).quoteAddLiquidity(token0, token1, true, token0Out, token1Out);
+            quote0 = quote0 * 1e18 / token0Decimals;
+            quote1 = quote1 * 1e18 / token1Decimals;
+            uint256 ratio = token0Out * 1e18 / token1Out * quote1 / quote0;
+            uint256 token0In = fromAmount * 1e18 / (ratio + 1e18);
+            uint256 token1In = fromAmount - token0In;
+
+            uint256 amountOutToken0 =
+                DexLibrary.swap(token0In, address(rewardToken), token0, IPair(swapPairToken0), swapFeeToken0);
+            uint256 amountOutToken1 =
+                DexLibrary.swap(token1In, address(rewardToken), token1, IPair(swapPairToken1), swapFeeToken1);
+
+            toAmount = DexLibrary.addLiquidity(address(depositToken), amountOutToken0, amountOutToken1);
+        } else {
+            toAmount = DexLibrary.convertRewardTokensToDepositTokens(
+                fromAmount,
+                address(rewardToken),
+                address(depositToken),
+                IPair(swapPairToken0),
+                swapFeeToken0,
+                IPair(swapPairToken1),
+                swapFeeToken1
+            );
+        }
     }
 
     function totalDeposits() public view override returns (uint256) {
