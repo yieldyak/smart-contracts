1f98e8432d719bac18ba3a0d3325e851ed3cb7d3
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2024-05-29 11:30:08 +0200

    add StargateV2NativeStrategy

--- contracts/strategies/crosschain/stargate/interfaces/IStargatePool.sol
+++ contracts/strategies/crosschain/stargate/interfaces/IStargatePool.sol
@@ -3,5 +3,6 @@ pragma solidity 0.8.13;
 
 interface IStargatePool {
     function token() external view returns (address);
-    function deposit(address receiver, uint256 amount) external returns (uint256);
+    function deposit(address receiver, uint256 amount) external payable returns (uint256);
+    function sharedDecimals() external view returns (uint8);
 }
