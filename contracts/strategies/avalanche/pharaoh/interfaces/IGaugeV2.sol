9c6fe7ae6a55acc4f634bc314f5b5ca9c2bc5ac6
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2025-10-22 10:49:00 +0200

    add PharStrategy

---
+++ contracts/strategies/avalanche/pharaoh/interfaces/IGaugeV2.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IGaugeV2 {
+    function deposit(uint256 amount) external;
+    function withdraw(uint256 amount) external;
+    function withdrawAll() external;
+    function earned(address token, address account) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function getReward(address account, address[] memory tokens) external;
+}
